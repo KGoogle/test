a5f9b40f0dcfd63b1b0230c136977b04e9af2728
KGoogle <142602443+KGoogle@users.noreply.github.com>
2026-01-05 16:20:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ RSS_SOURCES = {
         "AI 타임스": "https://cdn.aitimes.com/rss/gn_rss_allArticle.xml",
         "인공지능신문": "https://www.aitimes.kr/rss/S1N2.xml",
         "AI Matters": "https://aimatters.co.kr/category/news-report/ai-report/feed/",
-        "GeekNews": "https://feeds.feedburner.com/geeknews-feed"
+        "GeekNews(뉴스가 아닌것도 있음)": "https://feeds.feedburner.com/geeknews-feed"
     }
 }
 
@@ -651,4 +651,5 @@ if __name__ == "__main__":
         f.write(html_out)
         
     print("index.html Generated Successfully.")
+
 
